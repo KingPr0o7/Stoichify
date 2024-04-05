b05fcd923fbf6173fd18a7449f32a36ad0fd85de
Nathan Parker <natecoleparker@gmail.com>
2024-04-05 00:28:35 -0400

    🧮 Stoichiometric Value Gathering
    - Improved the element scanner by removing pointers, and moving to indexs.
    - Now using string_dilation to traverse a string when gathering subscripts.
    - Ensured range is withheld.
    - Now accounts for parentheses (multipliers)
    - Correctly calculates the element's subscript (amount of there of) by multiplying the coefficient, subscript, and multiplier (all if found, will have a different value from default, 1).

--- logic.py
+++ logic.py
@@ -1,10 +1,11 @@
 #
 # Outline
-#   1. Categorize and split the chemical equation
-#	2. Gather stoichiometric coefficients
+#   1. Categorize and split the chemical equation [DONE]
+#	2. Gather stoichiometric coefficients [DONE]
 # 	3. Balance the chemical equation
 # 	4. Apply Mole Ratios
-# 	5. Other (LR, Y)
+# 	5. Other (Limiting Reactant, Percent (%) Yield)
+#	6. Error Detection
 #
 
 #
@@ -20,9 +21,11 @@ import re # Regular Expressions
 #   Required scientific numbers used to bridge needed calculations.
 #
 
+DEBUG_MODE = True
 STP = 22.4 # Standard Temperature and Pressure (L/mol)
 AVOGADRO = 6.022 * 10**23 # Avogadro's number (particles/mol)
-ALPHABET = "^[a-zA-Z]$"
+UPPERCASE = "^[A-Z]$"
+LOWERCASE = "^[a-z]$"
 
 #
 # Formula Chemical Equation
@@ -35,65 +38,76 @@ chemical_equation = {
 	"products": {}
 }
 
-def element_finder(index, pointer, char, substance):
-	element = ""
-	if str(substance[index + 1]).islower():
-		element = f"{char}{substance[index + 1]}"
-		pointer += 2
-	else:
-		if re.match(ALPHABET, char):
-			print(element)
-			pointer += 1  
-	element = substance[pointer]
-	return pointer, element
-       
-def substance_collector(side, substance_list):
-	for substance in substance_list:
-		element_coefficient = substance[0]
-	
-		if re.match(ALPHABET, element_coefficient):
-			element_coefficient = 1
-
-		pointer = 0
-
-		for index, char in enumerate(substance): 
-			if pointer >= len(substance):
-				break
+def element_scanner(index, substance):
+	"""
+    Takes a string (AKA a substance) and first scans for a capital character.
+    Upon finding a capital letter, it checks the next character's capitalization state 
+    to see if it's a two-lettered element. If the second character isn't a lowercase 
+    character it's a single element. 
+    """
+	element = "-"
+	if index <= len(substance): # Ensure within range
+		if re.match(UPPERCASE, substance[index]):
+			element = substance[index] # Assume it's a single-lettered element
+			if index + 1 < len(substance) and re.match(LOWERCASE, substance[index + 1]): # Check the next character's capitalization state (keeping in range)
+				element = f"{substance[index]}{substance[index + 1]}"
+	return element
 			
-			if substance[pointer] != "(":
-				pointer, element = element_finder(index, pointer, char, substance)
-			elif substance[pointer] == ")":
-				pointer = len(substance)
-			else:
-				pointer += 1
-				pointer, element = element_finder(index, pointer, char, substance)
-				element_multiplier = int(substance[-1])
-   
-			# if pointer < len(substance):
-			# 	print(substance[pointer])
-   
-			element_subscript = 1
-			if pointer < len(substance) - 1 and str(substance[pointer]).isdigit():
-				element_subscript = substance[pointer]
-				pointer += 1
-			# print(f"{element}({element_subscript})")
     
-			# 	element_amount = int(element_coefficient) * int(element_subscript)
-			# 	# print(f"[{element_coefficient}]{element}({element_subscript}) = {element_amount}")
-			# 	chemical_equation[f"{side}"][element] = element_amount   
+def substance_scanner(side, substance_list):
+	"""
+    Loops through one side (reactants or products) of a chemical equation,
+    checking each substance's coefficient (always placed at the 0th index). 
+    Then, loops within such side (a substance) to perform element scans and
+    subscript calculations to be added the chemical_equation dictionary. 
+    """
+	for substance in substance_list:
+		# Make variables not local
+		substance_coefficient = substance[0] # Get the coefficient at the 0th index
+		element_multiplier = 1
+     
+		if re.match(UPPERCASE, substance_coefficient): # When not provided, a substance has a coefficient of 1.
+			substance_coefficient = 1	
+		elif str(substance_coefficient).isdigit(): # When provided
+			substance_coefficient = int(substance[0])
+	
+		for index, char in enumerate(substance):
+			# Make variables not local
+			element_subscript = 1 # Same as coefficients; if not provided; it's one.
+			string_dilation = 1 # Used to traverse the string
+	
+			if char == "(": # If a set of parentheses is detected, it has to have a subscript (multiplier)  
+				element_multiplier = int(substance[-1])
+				continue 
+			elif char != "(" and char != ")":
+				element = element_scanner(index, substance)
+				if element == "-": # Ignore elements that aren't found (trust me bro, the scanner works (at least I hope))
+					continue
+				else:
+					if len(element) == 2: # Move pointer over 2, to get to the end of the two-lettered elements
+						string_dilation = 2
+					if index + 1 < len(substance) and str(substance[index + string_dilation]).isdigit(): # Check if next char is a number, while in range.
+						element_subscript = substance_coefficient * int(substance[index + string_dilation]) * element_multiplier # Account for the coefficients, subscript, and parentheses (multipliers). 
+						if DEBUG_MODE == True:
+							if index + 1 < len(substance):
+								print(f"{element}({element_subscript}) [Coefficient: {substance_coefficient} * Subscript: {str(substance[index + string_dilation])} * Multiplier: {element_multiplier}]")
+					else: # If not a number, the subscript is 1.
+						if DEBUG_MODE == True:
+							print(f"{element}({element_subscript}) [Coefficient: {substance_coefficient} * Subscript: {element_subscript} * Multiplier: {element_multiplier}]")
+						element_subscript = substance_coefficient * element_subscript * element_multiplier # Account for the coefficients, subscript (1), and parentheses (multipliers).
+					chemical_equation[f"{side}"][element] = element_subscript  
 
 def chemical_equation_balancer(equation):
 	chemical_equation["string"] = str(equation)
-	# print(chemical_equation["string"].split(" "))
 
 	reactants = chemical_equation["string"].replace(" ", "").split("->")[0].split("+")
 	products = chemical_equation["string"].replace(" ", "").split("->")[1].split("+")
 	print(f"Reactants: {reactants}", "->", f"Products: {products}", "\n")
 
-	substance_collector("reactants", reactants)
-	substance_collector("products", products)
+	substance_scanner("reactants", reactants)
+	substance_scanner("products", products)
 
 
-chemical_equation_balancer("AgI + Fe2(CO3)3 -> FeI3 + Ag2CO3")
+chemical_equation_balancer("2AgI + 2Fe2(CO3)3 -> FeI3 + Ag2CO3")
 print(f"Reactant Substances: {chemical_equation['reactants']}")
 print(f"Product Substances: {chemical_equation['products']}")
