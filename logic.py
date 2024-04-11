9f48c2cbf79cafaac5edc6b3700349be42bc3f5a
Nathan Parker <natecoleparker@gmail.com>
2024-04-11 01:41:26 -0400

    🎚️ Balancing Chemical Equations
    - Added more "objects" to the chemical_equation (like unblanced/balanced, matrix, and elements).
    - Added a list of all unique elements (used for making the matrix)
    - Now accounts for complex ions (brackets [])
    - matrix_builder forms the matrix via the list of elements and amounts
    - Added docstrings to all functions
    - chemical_equation_balancer now balances chemical equations via logic by Mohammad-Ali Bandzar.
    - Balanced chemical equation is show back to the user in a valid way
    - Made use of debug_mode so console can be detailed, or not.
    - Organized entry and examples (unbalanced chemical equations).

--- logic.py
+++ logic.py
@@ -2,7 +2,7 @@
 # Outline
 #   1. Categorize and split the chemical equation [DONE]
 #	2. Gather stoichiometric coefficients [DONE]
-# 	3. Balance the chemical equation
+# 	3. Balance the chemical equation [DONE]
 # 	4. Apply Mole Ratios
 # 	5. Other (Limiting Reactant, Percent (%) Yield)
 #	6. Error Detection
@@ -10,19 +10,20 @@
 
 #
 # Imports
-#   Libaries or modules that are required and used
+#   Libraries or modules that are required and used
 #   in this module.
 #
 
 import re # Regular Expressions (String Checking)
-import fractions # Fraction Utils (Finding the Greatest Common Denominator (GCD))
+import sympy # Symbolic Mathematics (Equations)
+from sympy import Matrix, lcm # Matrix Operations, Least Common Multiple
 
 #
 # Constants
 #   Required scientific numbers used to bridge needed calculations.
 #
 
-DEBUG_MODE = True
+DEBUG_MODE = False # Toggle to True to see the properties of the chemical equation 
 STP = 22.4 # Standard Temperature and Pressure (L/mol)
 AVOGADRO = 6.022 * 10**23 # Avogadro's number (particles/mol)
 UPPERCASE = "^[A-Z]$"
@@ -34,9 +35,12 @@ LOWERCASE = "^[a-z]$"
 #
 
 chemical_equation = {
-	"string": "",
-	"reactants": [],
-	"products": []
+	"unbalanced": "", # User's inputted chemical equation
+	"elements": [], # All elements (uniques) within the chemical equation
+	"reactants": {}, # Reactants and their elements
+	"products": {}, # Products and their elements
+	"element_matrix": [], # Amounts of each substance into a matrix
+	"balanced": "" # Balanced chemical equation
 }
 
 def element_scanner(index, substance):
@@ -52,6 +56,9 @@ def element_scanner(index, substance):
 			element = substance[index] # Assume it's a single-lettered element
 			if index + 1 < len(substance) and re.match(LOWERCASE, substance[index + 1]): # Check the next character's capitalization state (keeping in range)
 				element = f"{substance[index]}{substance[index + 1]}"
+	# Add element to the list of elements to build the matrix
+	if element != "-" and element not in chemical_equation["elements"]:
+		chemical_equation["elements"].append(element)
 	return element
 			
     
@@ -63,22 +70,25 @@ def substance_scanner(side, substance_list):
     subscript calculations to be added the chemical_equation dictionary. 
     """
 	for substance in substance_list:
-		# Make variables not local
 		substance_coefficient = substance[0] # Get the coefficient at the 0th index
-		element_multiplier = 1
+		element_multiplier = 1 # If parentheses aren't detected, the subscript is 1 by default
      
-		if re.match(UPPERCASE, substance_coefficient): # When not provided, a substance has a coefficient of 1.
+		if re.match(UPPERCASE, substance_coefficient): # When not provided, a substance has a coefficient of 1
 			substance_coefficient = 1	
 		elif str(substance_coefficient).isdigit(): # When provided
 			substance_coefficient = int(substance[0])
 	
+		chemical_equation[side][substance] = [] # Create a list for each substance in the side
+ 
 		for index, char in enumerate(substance):
-			# Make variables not local
-			element_subscript = 1 # Same as coefficients; if not provided; it's one.
+			element_subscript = 1 # Same as coefficients; if not provided; it's 1.
 			string_dilation = 1 # Used to traverse the string
-	
+
 			if char == "(": # If a set of parentheses is detected, it has to have a subscript (multiplier)  
-				element_multiplier = int(substance[-1])
+				if substance[-1] == "]": # If the last character is a closing bracket, the subscript is the second to last character (the reactant is then a complex ion)
+					element_multiplier = int(substance[-2])
+				else:			
+					element_multiplier = int(substance[-1])
 				continue 
 			elif char != "(" and char != ")":
 				element = element_scanner(index, substance)
@@ -87,51 +97,116 @@ def substance_scanner(side, substance_list):
 				else:
 					if len(element) == 2: # Move pointer over 2, to get to the end of the two-lettered elements
 						string_dilation = 2
-						if string_dilation + index > len(substance) - 1:
+						if string_dilation + index > len(substance) - 1: # Ensure range
 							string_dilation = 1
 					if index + 1 < len(substance) and str(substance[index + string_dilation]).isdigit(): # Check if next char is a number, while in range.
 						element_subscript = int(substance[index + string_dilation])
 					else: # If not a number, the subscript is 1.
 						element_subscript = element_subscript
-					# print(element, substance_coefficient, element_subscript, element_multiplier)
 					element_data = (substance_coefficient, element_subscript, element_multiplier)
-					chemical_equation[side].append((element, element_data))
+					chemical_equation[side][substance].append((element, element_data))
+
+def matrix_builder(side):
+	"""
+	Creates a matrix of each element's amount (subscript * multiplier) in each substance of the side
+	(reactants or products (-1 to distinguish between the two)). 
+	"""
+	for substance in chemical_equation[side]:
+		matrix_row = []
+		for element in chemical_equation["elements"]:
+			found = False
+			for substance_element in chemical_equation[side][substance]:
+				if substance_element[0] == element:
+					if side == "reactants":
+						matrix_row.append(substance_element[1][1] * substance_element[1][2])
+					else:
+						matrix_row.append(substance_element[1][1] * substance_element[1][2] * -1)
+					found = True
+					break
+			if not found:
+				matrix_row.append(0)
+		chemical_equation["element_matrix"].append(matrix_row)    
 
 def chemical_equation_balancer(equation):
-	chemical_equation["string"] = str(equation)
+	"""
+	Splits reactants and products, scans for elements, builds a matrix, solves the matrix, 
+	balances the chemical equation, and saves results as a valid chemical equation. Balancing logic 
+	wouldn't be possible without the use of matrices and linear algebra by Mohammad-Ali Bandzar -
+	(https://medium.com/swlh/balancing-chemical-equations-with-python-837518c9075b).
+	"""
+	chemical_equation["unbalanced"] = str(equation)
 
-	reactants = chemical_equation["string"].replace(" ", "").split("->")[0].split("+")
-	products = chemical_equation["string"].replace(" ", "").split("->")[1].split("+")
-	print(f"Reactants: {reactants}", "->", f"Products: {products}", "\n")
+	reactants = chemical_equation["unbalanced"].replace(" ", "").split("->")[0].split("+")
+	products = chemical_equation["unbalanced"].replace(" ", "").split("->")[1].split("+")
+
+	if DEBUG_MODE == True:
+		print(f"\nReactants: {reactants}", "->", f"Products: {products}", "\n")
 
 	substance_scanner("reactants", reactants)
 	substance_scanner("products", products)
 
-	variables = {}
+	if DEBUG_MODE == True:
+		print(f"All Elements Present: {chemical_equation['elements']} ({len(chemical_equation['elements'])})\n")
+
+	matrix_builder("reactants")
+	matrix_builder("products")	  
+
+	# Use smypy to solve the matrix via linear algebra (Thanks to Mohammad-Ali Bandzar for this logic/code)
+	matrix = Matrix(chemical_equation["element_matrix"])
+	matrix = matrix.transpose() # Swap the rows and columns
+	solution = matrix.nullspace()[0] # Solve the matrix
+	multiple = lcm([val.q for val in solution]) # Find the least common multiple (LCM) of the denominators 
+	balanced_coefficients = multiple * solution # Multiply by LCM to remove fractions (either solution is valid)
+
+	if DEBUG_MODE == True:
+		print(f"Chemical Matrix: {chemical_equation['element_matrix']} = {balanced_coefficients}\n")
+
+	reactants.append("->")
+	substances = reactants + products
+	pointer = 0
+
+	for_show_balanced = ""
  
-	for reactant_element in chemical_equation['reactants']:
-		reactant_element_amount = reactant_element[1][1] * reactant_element[1][2]
-		if reactant_element[0] not in variables:
-			variables[reactant_element[0]] = [reactant_element_amount]
+	for index, substance in enumerate(substances):
+		if substance != "->":
+			if index == len(substances) - 1:
+				chemical_equation["balanced"] += f"{balanced_coefficients[pointer]}{substance}"
+				for_show_balanced += f"\033[1m{balanced_coefficients[pointer]}\033[0m{substance}"
+			else:
+				if substances[index + 1] != "->":
+					chemical_equation["balanced"] += f"{balanced_coefficients[pointer]}{substance} + "
+					for_show_balanced += f"\033[1m{balanced_coefficients[pointer]}\033[0m{substance} + "
+				else:
+					chemical_equation["balanced"] += f"{balanced_coefficients[pointer]}{substance} "
+					for_show_balanced += f"\033[1m{balanced_coefficients[pointer]}\033[0m{substance} "
+			pointer += 1
 		else:
-			variables[reactant_element[0]].append(reactant_element_amount)
- 
-	for product_element in chemical_equation['products']:
-		product_element_amount = product_element[1][1] * product_element[1][2]
-		if product_element[0] not in variables:
-			variables[product_element[0]] = [product_element_amount]
-		else:
-			variables[product_element[0]].append(product_element_amount)  
+			chemical_equation["balanced"] += "-> " 
+			for_show_balanced += "-> "
 
-	known_variable = 1 # The known variable (usually referred to as a) is always going to be 1, to start the chain reactions (pun intended) to solve the unknowns.
+	if DEBUG_MODE == True:
+		print(f"\nUnbalanced Chemical Equation: {chemical_equation['unbalanced']}")
+	print(f"Balanced Chemical Equation: {for_show_balanced}")
 
-# for var in variables:
-# 	fraction = fractions.Fraction(var).limit_denominator()
-	# print(var, fraction)
+#
+# Examples of Unbalanced Chemical Equations
+#
+# AgI + Fe2(CO3)3 -> FeI3 + Ag2CO3 
+# KMnO4 + HCl -> MnCl2 + KCl + Cl2 + H2O
+# Al + O2 -> Al2O3
+# C2H4 + O2 -> CO2 + H2O
+# Ca3(PO4)2 + SiO2 + C -> CaSiO3 + P4 + CO
+# NH3 + O2 -> NO + H2O
+# NaOH + H2S04 -> Na2S04 + H20
+# Ca(OH)2 + H3PO4 -> Ca3(PO4)2 + H2O
+# K4[Fe(SCN)6] + K2Cr2O7 + H2SO4 -> Fe2(SO4)3 + Cr2(SO4)3 + CO2 + H2O + K2SO4 + KNO3 (Hardest I could find)
+# Zn + HNO3 -> Zn(NO3)2 + H2O + N2O
 
-	print(variables)
-
-     
-chemical_equation_balancer("Ca3(PO4)2 + SiO2 + C -> CaSiO3 + P4 + CO") # AgI + Fe2(CO3)3 -> FeI3 + Ag2CO3 | KMnO4 + HCl -> MnCl2 + KCl + Cl2 + H2O | Al + O2 -> Al2O3 | C2H4 + O2 -> CO2 + H2O
-print(f"Reactant Substances: {chemical_equation['reactants']}")
-print(f"Product Substances: {chemical_equation['products']}")
+if __name__ == "__main__":
+	if DEBUG_MODE == True:
+		chemical_equation_balancer("Zn + HNO3 -> Zn(NO3)2 + H2O + N2O") # Insert your chemical equation here
+		print(f"\nReactant Substances: {chemical_equation['reactants']}")
+		print(f"Product Substances: {chemical_equation['products']}") 
+	else:
+		user_chemical_equation = input("Enter a chemical equation: ")
+		chemical_equation_balancer(user_chemical_equation)
