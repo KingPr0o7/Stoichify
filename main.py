a6120ba62249cd49ef4f95c4e915d1353f36a62d
Nathan Parker <natecoleparker@gmail.com>
2024-05-08 21:23:44 -0400

    🖼️ GUI Save
    - Added exit button
    - Added a "settings" button, might be changed
    - Centered the "shown work" section

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ class MainWindow:
 	def __init__(self):
 		self.window = tk.Tk()
 		self.window.title("Stoichify")
-		self.window.geometry("1000x500")
+		self.window.geometry("1000x700")
 		
 		self.substances = []
 		# ---------------
@@ -22,9 +22,36 @@ class MainWindow:
 
 		self.equation = tk.StringVar()
 
-		self.navbar = ttk.Frame(self.window)
-		self.navbar.pack(side="top", fill="x")
+		self.navbar = tk.Frame(self.window, height=100)
+		self.navbar.pack(side="top", fill="x", padx=15)
 
+		lines = tk.Frame(self.navbar, height=1, width=500)
+		lines.pack(side=tk.BOTTOM, fill="x", expand=True)
+		
+		line1 = tk.Frame(lines, height=1, width=425, bg="white")
+		line1.pack(side=tk.LEFT, fill="x", expand=True)
+		
+		self.exit = tk.Button(lines, text="Exit", command=self.window.quit, highlightthickness = 0, bd = 0, bg="red", fg="white", height=1, font=("Times New Roman", 13), width=10)
+		self.exit.pack(side="left", padx=5)
+		
+		line2 = tk.Frame(lines, height=1, width=425, bg="white")
+		line2.pack(side=tk.RIGHT, fill="x", expand=True)
+
+		self.iconPath = 'images/stoichify_logo.png'
+		self.icon = ImageTk.PhotoImage(Image.open(self.iconPath).resize((200, 81)))
+		self.icon_size = tk.Label(self.navbar)
+		self.icon_size.image = self.icon  # <== this is were we anchor the img object
+		self.icon_size.configure(image=self.icon)
+		self.icon_size.pack(side=tk.LEFT)
+
+		self.settings_path = 'images/settings.png'
+		self.settings = ImageTk.PhotoImage(Image.open(self.settings_path).resize((32, 32)))
+		self.settings_size = tk.Button(self.navbar,command=lambda: messagebox.showinfo("Settings", "Settings are not available yet."), highlightthickness = 0, bd = 0)
+		self.settings_size.image = self.settings  # <== this is were we anchor the img object
+		self.settings_size.configure(image=self.settings)
+		self.settings_size.pack(side=tk.RIGHT)
+
+		# ---
 		self.user_input = ttk.Frame(self.window, height=10)
 		self.user_input.pack(side="bottom", pady=15)
 
@@ -37,18 +64,18 @@ class MainWindow:
 
 	def create_fraction(self, frame, numerator_text, denominator_text): 
 		fraction_holder = tk.Frame(frame)
-		fraction_holder.pack(side=tk.LEFT, padx=5)
-     
+		fraction_holder.pack(side=tk.LEFT, padx=5, expand=True)
+	
 		max_length = max(len(numerator_text), len(denominator_text))
 	
 		numerator = tk.Label(fraction_holder, text=numerator_text, font=("Times New Roman", 20), width=max_length)
-		numerator.pack(side=tk.TOP)
+		numerator.pack(side=tk.TOP, anchor='center')
 	
 		line = tk.Frame(fraction_holder, height=1, width=200, bg="white")
-		line.pack(side=tk.TOP, fill=tk.X)
+		line.pack(side=tk.TOP, fill=tk.X, anchor='center')
 	
 		denominator = tk.Label(fraction_holder, text=denominator_text, font=("Times New Roman", 20), width=max_length)
-		denominator.pack(side=tk.TOP)
+		denominator.pack(side=tk.TOP, anchor='center')
 
 	def balance_equation(self, equation):
 		"""
@@ -59,8 +86,8 @@ class MainWindow:
 		showing_chemical_equation = tk.Label(self.window, text=self.equation.balanced, font=("Times New Roman", 20))
 		showing_chemical_equation.pack()
 
-		self.work_shown_wrapper = tk.Frame(self.window)
-		self.work_shown_wrapper.pack(fill="x", pady=15, anchor="center")
+		self.work_shown_wrapper = tk.Frame(self.window, width=500)
+		self.work_shown_wrapper.pack(pady=15, anchor="center")
 
 		self.equation_input.destroy()
 		self.given_amount = ttk.Entry(self.user_input, width=25)
